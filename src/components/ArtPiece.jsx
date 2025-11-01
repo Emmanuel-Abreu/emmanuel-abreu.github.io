f7d17c2b5e09a78804819bd9243eda552fe616a0
Manny A <28583353+Emmanuel-Abreu@users.noreply.github.com>
2025-11-01 17:09:23 -0400

    Created prop values for ArtPieces

--- src/components/ArtPiece.jsx
+++ src/components/ArtPiece.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import '../assets/styles/ArtPiece.css'
 
 export default function ArtPiece(props){
-  
+  const src = props.src;
+  const title = props.title;
+  const altText = props.altText;
+  const description = props.description;
+
   return (
     <div className="ArtPiece">
-      <p>ArtWork</p>
+      <img src={src} alt={altText} />
+      <p>{title}</p>
     </div>
   );
 }
